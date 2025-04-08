964fb47f26b1f5e23b0e11ca37329f8ad6172070
CGJI58 <leemsk22@naver.com>
2025-04-08 16:55:33 +0900

    header 디자인 수정: 색 변화 삭제 & scrollMeter 추가

---
+++ src/Components/ScrollMeter.tsx
@@ -0,0 +1,75 @@
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { throttle } from "lodash";
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+
+function ScrollMeter() {
+  const { scrollYProgress } = useScroll();
+  const [scrollProgress, setScrollProgress] = useState<number>(0);
+  const [innerHeight, setInnerHeight] = useState<number>(window.innerHeight);
+  const [scrollHeight, setScrollHeight] = useState<number>(
+    document.documentElement.scrollHeight
+  );
+  const [noScroll, setNoScroll] = useState<boolean>(true);
+
+  const throttledScroll = useRef(
+    throttle(
+      (progressValue: number) => {
+        const progress = Math.floor(progressValue * 100);
+        setScrollProgress(progress);
+      },
+      200,
+      { leading: false, trailing: true }
+    )
+  );
+
+  const observer = new MutationObserver(() => {
+    const currentHeight = document.documentElement.scrollHeight;
+    setScrollHeight(() => currentHeight);
+  });
+
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    characterData: true,
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (progressValue) => {
+    if (noScroll) {
+      throttledScroll.current(0);
+      setNoScroll(false);
+    } else {
+      throttledScroll.current(progressValue);
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    setInnerHeight(() => window.innerHeight);
+  });
+
+  useEffect(() => {
+    if (innerHeight === scrollHeight) {
+      setNoScroll(true);
+    }
+  }, [innerHeight, scrollHeight]);
+
+  return (
+    <Wrapper
+      scrollprogress={scrollProgress}
+      animate={{ width: `${scrollProgress}%` }}
+      transition={{ duration: 0.3, ease: "easeOut" }}
+    />
+  );
+}
+
+const Wrapper = styled(motion.div)<{ scrollprogress: number }>`
+  width: ${(props) => props.scrollprogress}%;
+  height: 2px;
+  left: 0px;
+  bottom: 0px;
+  background-color: ${(props) => props.theme.highlight};
+  position: absolute;
+`;
+
+export default ScrollMeter;
--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -2,85 +2,86 @@ import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { IUserState, userState } from "../States/atoms";
-import {
-  motion,
-  useAnimation,
-  useMotionValueEvent,
-  useScroll,
-} from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import UserInfo from "./userInfo";
+import ScrollMeter from "./ScrollMeter";
 
 function Header() {
-  const { scrollY } = useScroll();
-  const navAnimation = useAnimation();
-  useMotionValueEvent(scrollY, "change", (scroll) => {
-    if (scroll > 20) {
-      navAnimation.start("scroll");
-    } else {
-      navAnimation.start("top");
-    }
-  });
   const user = useRecoilValue<IUserState>(userState);
 
   return (
-    <Wrapper variants={navVariants} initial="top" animate={navAnimation}>
-      <Link to="/">
-        <Col>Home</Col>
-      </Link>
+    <Wrapper>
+      <Col>
+        <StyledLink to="/">
+          <FontAwesomeIcon icon={faHouse} />
+        </StyledLink>
+      </Col>
       {user.userInfo?.email === "" ? null : (
-        <Link
-          to={{ pathname: "/write", search: "?mode=create" }}
-          state={{ diary: { id: "", date: "", title: "", text: "" } }}
-        >
-          <Col>Write</Col>
-        </Link>
+        <Col>
+          <StyledLink
+            to={{ pathname: "/write", search: "?mode=create" }}
+            state={{ diary: { id: "", date: "", title: "", text: "" } }}
+          >
+            Write
+          </StyledLink>
+        </Col>
       )}
       {user.userInfo?.email === "" ? (
-        <Link to="/login">
-          <Col>Log in</Col>
-        </Link>
+        <Col>
+          <StyledLink to="/login">Log in</StyledLink>
+        </Col>
       ) : (
-        <Link to="/logout">
-          <Col>Log out</Col>
-        </Link>
+        <Col>
+          <UserInfo />
+        </Col>
       )}
+      <ScrollMeter />
     </Wrapper>
   );
 }
 
-const navVariants = {
-  top: {
-    backgroundColor: "rgba(0, 0, 0, 0.2)",
-  },
-  scroll: {
-    backgroundColor: "rgba(0, 0, 0, 1)",
-  },
-};
-
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
   z-index: 99;
   position: fixed;
   top: 0;
   width: 100%;
   height: 50px;
+  background-color: ${(props) => props.theme.background.lighter};
   display: flex;
   justify-content: space-around;
   align-items: center;
-  font-size: 24px;
+  font-size: 1.5rem;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05), 4px 4px 8px rgba(0, 0, 0, 0.05);
   a {
+    cursor: default;
+    width: 100%;
     height: 100%;
     text-decoration: none;
-    color: whitesmoke;
   }
 `;
 
 const Col = styled.div`
-  width: 1fr;
-  padding: 0 10px;
+  width: 30%;
   height: 100%;
+  box-sizing: border-box;
   display: flex;
   justify-content: center;
   align-items: center;
-  cursor: pointer;
+  & > * {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${(props) => props.theme.text};
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  &:visited {
+    color: ${(props) => props.theme.text};
+  }
 `;
 
 export default Header;
