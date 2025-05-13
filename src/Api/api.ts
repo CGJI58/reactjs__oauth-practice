3e61eafec58d57e2325d45494365d6717fe31c89
CGJI58 <leemsk22@naver.com>
2025-05-13 15:18:04 +0900

    회원탈퇴 api 함수 추가 & 다른 api 함수들 코드 정리

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -3,7 +3,7 @@ import { IGetUserByCookie, IUserState } from "../types/types";
 
 const BE_BASE_URL = process.env.REACT_APP_BACK_END_URL;
 
-export const loginByGhCode = async (ghCode: string) => {
+export const loginByGhCode = async (ghCode: string): Promise<boolean> => {
   console.log("Run loginByGhCode()");
   try {
     const response = await fetch(`${BE_BASE_URL}/auth/login-by-ghcode`, {
@@ -18,7 +18,11 @@ export const loginByGhCode = async (ghCode: string) => {
       }),
     });
     const { ok } = response;
-    return ok;
+    if (ok) {
+      return ok;
+    } else {
+      throw new Error(`Server responded with status: ${response.status}`);
+    }
   } catch (error) {
     console.error("loginByGhCode:", error);
     return false;
@@ -32,18 +36,20 @@ export const getUserByCookie = async (): Promise<IGetUserByCookie> => {
       mode: "cors",
       credentials: "include",
     });
-    if (response.ok) {
+    const { ok, status } = response;
+    if (ok) {
       const userData: IUserState = await response.json();
-      return { status: response.status, userData };
+      return { status: status, userData };
     } else {
-      return { status: response.status, userData: defaultUserState };
+      return { status: status, userData: defaultUserState };
     }
   } catch (error) {
+    console.error("getUserByCookie:", error);
     return { status: null };
   }
 };
 
-export const deleteCookie = async () => {
+export const deleteCookie = async (): Promise<void> => {
   try {
     const response = await fetch(`${BE_BASE_URL}/auth/delete-cookie`, {
       method: "POST",
@@ -54,29 +60,73 @@ export const deleteCookie = async () => {
     const responseText = await response.text();
     if (ok) {
       console.log("Cookie has been deleted.");
+      return;
     } else {
-      console.error("fail to delete cookie on server:", status, responseText);
+      throw new Error(
+        `fail to delete user data on server: ${status} ${responseText}`
+      );
     }
   } catch (error) {
     console.error(error);
+    return;
   }
 };
 
-export const updateUser = async (user: IUserState) => {
-  const response = await fetch(`${BE_BASE_URL}/users/update`, {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ user }),
-  });
-  return response.ok;
+export const updateUser = async (user: IUserState): Promise<boolean> => {
+  try {
+    const response = await fetch(`${BE_BASE_URL}/users/update`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user }),
+    });
+    const { ok, status } = response;
+    if (ok) {
+      return ok;
+    } else {
+      throw new Error(`fail to delete user data on server: ${status}`); // 이거 메세지가 좀 잘못됐음. 수정할 것.
+      // 백엔드에서도 update 할 때 delete user 어쩌구 메세지였던거같은데
+      // 회원탈퇴기능이랑 헷갈리니까 이것도 찾아서 다 고쳐
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 };
 
-export const getCodeRequestURL = async () => {
-  const { codeRequestURL }: { codeRequestURL: string } = await (
-    await fetch(`${BE_BASE_URL}`)
-  ).json();
-  return codeRequestURL;
+export const deleteUser = async (user: IUserState): Promise<boolean> => {
+  try {
+    const response = await fetch(`${BE_BASE_URL}/auth/delete-user`, {
+      method: "DELETE",
+      mode: "cors",
+      credentials: "include",
+    });
+    const { ok, status } = response;
+    const responseText = await response.text();
+    if (ok) {
+      console.log("User data has been deleted.");
+      return ok;
+    } else {
+      throw new Error(
+        `fail to delete user data on server: ${status} ${responseText}`
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+export const getCodeRequestURL = async (): Promise<string> => {
+  try {
+    const { codeRequestURL }: { codeRequestURL: string } = await (
+      await fetch(`${BE_BASE_URL}`)
+    ).json();
+    return codeRequestURL;
+  } catch (error) {
+    console.error("getCodeRequestURL:", error);
+    return "";
+  }
 };
