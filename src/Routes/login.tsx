583a90973567591b44ce97add9f88c6cddad8dfc
CGJI58 <leemsk22@naver.com>
2025-05-14 14:23:11 +0900

    login: 팝업창 뒷배경 어둡게 변경(css)

--- src/Routes/login.tsx
+++ src/Routes/login.tsx
@@ -63,15 +63,14 @@ function Login() {
 
 const Wrapper = styled.div`
   z-index: 100;
-  position: absolute;
+  position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
 `;
 const Popup = styled.div`
   z-index: 101;
